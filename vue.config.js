b06c4e97f6698150680de9fa4458ea18ec98aa12
Aynzy1116 <Aynzy1116@163.com>
2023-02-18 15:56:12 +0800

    # 注释代理

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,27 @@
 module.exports = {
   devServer: {
     open: true, // 是否自动弹出浏览器页面
-    // host: 'localhost',
-    host: '192.168.137.1',
+    host: 'localhost',
+    // host: '192.168.137.1',
     port: '8080',
     https: false,
     hotOnly: false,
     // 配置代理实现跨域
-    proxy: {
-      '/proxy': {
-        target: 'http://106.52.29.126:8081', // API服务器的地址
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/proxy': ''
-        }
-      },
-      '/api': {
-        target: 'http://localhost:3001', // API服务器的地址
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      },
-      '/socket.io': {
-        target: 'http://localhost:3010', // socket服务器的地址
-        changeOrigin: true,
-        logLevel: 'debug'
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3001', // API服务器的地址
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   },
+    //   '/socket.io': {
+    //     target: 'http://localhost:3010', // socket服务器的地址
+    //     changeOrigin: true,
+    //     logLevel: 'debug'
+    //   }
+    // }
   },
   lintOnSave: false // 严格模式开启（默认true）或者关闭（false）
 }
